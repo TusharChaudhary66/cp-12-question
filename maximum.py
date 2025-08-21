9467aef3175385e99d4ac902f39792b375bae6cd
TusharChaudhary66 <tushar.chaudhary_bca24@gla.ac.in>
2025-08-21 08:15:30 +0530

    Add files via upload

---
+++ maximum.py
@@ -0,0 +1,13 @@
+A = int(input("Enter first number: "))
+B = int(input("Enter second number: "))
+C = int(input("Enter third number: "))
+
+if A<B and A<C:
+    print("A is maximum")
+elif B<A and B<C: 
+    print("B is maximum")
+else:
+    print("C is maximum")     
+
+
+
---
+++ week.py
@@ -0,0 +1,5 @@
+A=int(input("Enter a number"))
+if A==1:
+    print("")
+
+
