35940747b5e493119550cd51856cf366f464cc10
TusharChaudhary66 <tushar.chaudhary_bca24@gla.ac.in>
2025-08-24 16:14:11 +0530

    Update Validtriangle.py

---
+++ Validtriangle.py
@@ -0,0 +1,7 @@
+A = int(input("Enter first number: "))
+B = int(input("Enter second number: "))
+C = int(input("Enter third number: "))
+if A+B+C == 180:
+    print("Valid triangle")
+else:
+    print("Invalid triangle")
